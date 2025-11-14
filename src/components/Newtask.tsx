0ab7208b72f458d72346a3ea9c94aaa9455b461b
Neel-SoftwarePRYM <embeddedprym@gmail.com>
2025-11-14 15:47:02 +0530

    complted the app

--- src/components/Newtask.tsx
+++ src/components/Newtask.tsx
@@ -1,24 +1,31 @@
-import { View, Text, TouchableOpacity, TextInput } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, Keyboard } from 'react-native';
 import React, { useState } from 'react';
 import { Plus } from 'lucide-react-native';
 
-const Newtask = () => {
+type Props = {
+  onAdd: (text: string) => void;
+};
+
+const Newtask: React.FC<Props> = ({onAdd}) => {
   const [task, setTask] = useState('');
-  const [tasks, setTasks] = useState<string[]>([]);
 
   const handleAdd = () => {
-    if (!task.trim()) return;
-    setTasks(prev => [...prev, task.trim()]);
-    setTask('');
-  };
+    const trimmed = task.trim();
+    if(!trimmed) return;
+    onAdd(trimmed)
+    setTask('')
+    Keyboard.dismiss();
+  }
 
   return (
-    <View className="flex flex-row items-center justify-center mt-4 gap-3">
+    <View className="flex flex-row items-center justify-center mt-5 gap-3">
       <TextInput
         className="bg-gray-300/50 rounded-xl h-12 px-3 border border-gray-400/40 w-3/5"
         placeholder="Add New Task"
         value={task}
         onChangeText={setTask}
+        returnKeyType='done'
+        onSubmitEditing={handleAdd}
       />
       <TouchableOpacity
         onPress={handleAdd}
--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,25 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
 
-const cards = () => {
+type Props = {
+  total: number;
+  completed: number;
+  pending: number;
+};
+
+const cards: React.FC<Props> = ({ total, completed, pending }) => {
   return (
     <View className="flex space-x-2 flex-row gap-5 items-center justify-center mt-8 mx-6">
-              <View className="bg-blue-300/30 p-5 rounded-xl text-center items-center flex-1">
-                <Text className="font-bold text-3xl text-blue-700">12</Text>
-                <Text className="text-1xl font-semibold text-blue-700">Total</Text>
-              </View>
-              <View className="bg-green-300/30 p-5 rounded-xl text-center items-center flex-1">
-                <Text className="font-bold text-3xl text-green-700">8</Text>
-                <Text className="text-1xl font-semibold text-green-700">Done</Text>
-              </View>
-              <View className="bg-orange-300/30 p-5 rounded-xl text-center items-center flex-1">
-                <Text className="font-bold text-3xl text-orange-700">4</Text>
-                <Text className="text-1xl font-semibold text-orange-700">
-                  Pending
-                </Text>
-              </View>
-            </View>
-  )
-}
+      <View className="bg-blue-300/30 p-5 rounded-xl text-center items-center flex-1">
+        <Text className="font-bold text-3xl text-blue-700">{total}</Text>
+        <Text className="text-1xl font-semibold text-blue-700">Total</Text>
+      </View>
+      <View className="bg-green-300/30 p-5 rounded-xl text-center items-center flex-1">
+        <Text className="font-bold text-3xl text-green-700">{completed}</Text>
+        <Text className="text-1xl font-semibold text-green-700">Done</Text>
+      </View>
+      <View className="bg-orange-300/30 p-5 rounded-xl text-center items-center flex-1">
+        <Text className="font-bold text-3xl text-orange-700">{pending}</Text>
+        <Text className="text-1xl font-semibold text-orange-700">Pending</Text>
+      </View>
+    </View>
+  );
+};
 
-export default cards
+export default cards;
